075e4c7365a2f4a725d473bb5c909f08ad68c5c8
Alucard Stormbringer <supersayan1000@gmail.com>
2024-10-28 10:26:19 +0530

    add explanations of codes used in experiments

--- Experiment_1.cpp
+++ Experiment_1.cpp
@@ -1,33 +1,56 @@
 /*
 Aim: Write a program to Implement stack using array.
 
-Stack uses First in last out method, In stack all the operations are performed from the top.
-we will use push function to insert element and pop function to delete element.
-display function is used for displaying the current elements stored in the stack.
+Stack is a linear data structure that follows the Last In First Out (LIFO) principle.
+This means that the last element inserted into the stack is the first one to be removed.
+Think of it like a stack of plates - you can only add or remove plates from the top.
 
-DO while loops is being used to show menu and ask the user which task needs to be performed on the stack.
+Key Operations:
+1. Push - Add an element to the top of stack
+2. Pop - Remove the topmost element from stack
+3. Display - Show all elements currently in stack
+
+Key Variables Used:
+- stack[100]: Array to store stack elements 
+- top: Index pointing to top of stack (-1 when empty)
+- n: Maximum size of stack (user-defined, max 100)
+- choice: Store user's menu choice
+- x: Temporary variable to store element being pushed
+
+The program implements a menu-driven interface using do-while loop that allows users to:
+1. Push elements onto stack
+2. Pop elements from stack  
+3. Display all elements
+4. Exit the program
 */
 
 #include<iostream>
 
 using namespace std;
 
-// Define variables
+// Define global variables accessible to all functions
 int stack[100],choice,n,top,x,i;
 
-// Define empty Functions
-void push(void);
-void pop(void);
-void display(void);
+// Function declarations (prototypes)
+void push(void);    // Add element to stack
+void pop(void);     // Remove element from stack
+void display(void); // Show stack elements
 
 int main()
 {
+    // Initialize top as -1 to indicate empty stack
     top=-1;
+    
+    // Get stack size from user (maximum 100)
     cout<<"\n Enter the size of STACK[MAX=100]:";
     cin>>n;
+    
+    // Display menu options
     cout<<"\n\t STACK OPERATIONS USING ARRAY";
     cout<<"\n\t--------------------------------";
     cout<<"\n\t 1.PUSH\n\t 2.POP\n\t 3.DISPLAY\n\t 4.EXIT";
+    
+    // Menu loop continues until user chooses to exit
     do
     {
         cout<<"\n Enter the Choice:";
@@ -36,17 +59,17 @@ int main()
         {
             case 1:
             {
-                push();
+                push();    // Call push function
                 break;
             }
             case 2:
             {
-                pop();
+                pop();     // Call pop function
                 break;
             }
             case 3:
             {
-                display();
+                display(); // Call display function
                 break;
             }
             case 4:
@@ -65,42 +88,47 @@ int main()
     return 0;
 }
 
-// Function to insert element
+// Function to insert element (Push operation)
 void push()
 {
+    // Check if stack is full
     if(top>=n-1)
     {
         cout<<"\n\tSTACK is over flow";
     }
     else
     {
+        // Get value from user and add to stack
         cout<<" Enter a value to be pushed:";
         cin>>x;
-        top++;
-        stack[top]=x;
+        top++;              // Increment top
+        stack[top]=x;       // Store value at top
     }
 }
 
-// Function to delete element
+// Function to delete element (Pop operation)
 void pop()
 {
+    // Check if stack is empty
     if(top<=-1)
     {
         cout<<"\n\t Stack is under flow";
     }
     else
     {
+        // Display and remove top element
         cout<<"\n\t The popped elements is "<<stack[top];
-        top--;
+        top--;             // Decrement top
     }
 }
 
-// Function to display the stack
+// Function to display all elements in stack
 void display()
 {
     if(top>=0)
     {
         cout<<"\n The elements in STACK \n";
+        // Loop from top to bottom of stack
         for(i=top; i>=0; i--)
             cout<<"\n"<<stack[i];
         cout<<"\n Press Next Choice";
--- Experiment_2.cpp
+++ Experiment_2.cpp
@@ -1,9 +1,21 @@
 /*
 Aim: Write a program to Implement Queue using Array.
 
-Queue uses First in First Out. In Queue the Insertion is performed on front and Deletion is performed on rear.
+Queue uses First in First Out (FIFO) principle. In a Queue, elements are inserted at the rear end 
+and deleted from the front end, similar to a real-world queue of people.
 
-DO while loops is being used to show menu and ask the user which task needs to be performed on the queue.
+This program implements a Queue using an array with the following operations:
+1. Insertion (Enqueue) - Add elements at rear end
+2. Deletion (Dequeue) - Remove elements from front end  
+3. Display - Show all elements currently in queue
+4. Exit - Terminate the program
+
+Key variables used:
+- queue[n]: Array to store queue elements
+- front: Index pointing to front of queue
+- rear: Index pointing to rear of queue
+- x: Tracks available space
+- n: Maximum size of queue (set to 5)
 */
 
 #include<iostream>
@@ -14,9 +26,14 @@ using namespace std;
 int main()
 {
     // Define Variables
+    // queue - array to store elements
+    // ch - store user's menu choice
+    // front,rear - track queue boundaries
+    // j - counter for input prompts
+    // x - tracks available space
     int queue[n],ch=1,front=0,rear=0,i,j=1,x=n;
 
-    // Menu for Queue
+    // Display menu options to user
     cout<<"Queue using Array";
     cout<<"\n1.Insertion \n2.Deletion \n3.Display \n4.Exit";
     
@@ -27,45 +44,45 @@ int main()
         switch(ch)
         {
 
-        // Insertion
+        // Case 1: Insert element at rear
         case 1:
             if(rear==x)
-                cout<<"\n Queue is Full";
+                cout<<"\n Queue is Full";  // Check if queue is full
             else
             {
                 cout<<"\n Enter no :"<<j++;
-                cin>>queue[rear++];
+                cin>>queue[rear++];  // Insert element and increment rear
             }
             break;
         
-        // Deletion
+        // Case 2: Delete element from front
         case 2:
             if(front==rear)
             {
-                cout<<"\n Queue is empty";
+                cout<<"\n Queue is empty";  // Check if queue is empty
             }
             else
             {
-                cout<<"\n Deleted Element is "<<queue[front++];
-                x++;
+                cout<<"\n Deleted Element is "<<queue[front++];  // Remove and display front element
+                x++;  // Increment available space
             }
             break;
         
-        // Display
+        // Case 3: Display all queue elements
         case 3:
             cout<<"\nQueue Elements are:\n ";
             if(front==rear)
                 cout<<"\n Queue is Empty";
             else
             {
-                for(i=front; i<rear; i++)
+                for(i=front; i<rear; i++)  // Loop from front to rear
                 {
                     cout<<queue[i];
                     cout<<"\n";
                 }
                 break;
         
-        // Exit
+        // Case 4: Exit program
         case 4:
                 exit(0);
             default:
